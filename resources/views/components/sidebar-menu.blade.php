cad5368214356d6d7ee354cb986713d5f8f0b3ea
MelissaArevaloL <melissa.arevalo@comfamiliarhuila.com>
2023-10-03 20:15:22 -0500

    commit meli

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -4,82 +4,70 @@
             <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
                 <a class="nav-item-hold" href="{{route('home')}}">
                     <i class="nav-icon escritorio"></i>
-                    <span class="nav-text">Escritorio</span>
+                    <span class="nav-text">Inicio</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-            @if (Auth::user()->is_admin)
-            <li class="nav-item {{ request()->is('users') ? 'active' : '' }}" >
-                <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon clientes"></i>
-                    <span class="nav-text">Usuarios</span>
-                </a>
-                <div class="triangle"></div>
-            </li>
-            @endif
             <li class="nav-item {{ request()->is('providers') ? 'active' : '' }}">
                 <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon proveedores"></i>
-                    <span class="nav-text">Proveedores</span>
+                    <i class="nav-icon convenios"></i>
+                    <span class="nav-text">Convenios</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-            <li class="nav-item {{ request()->is('servers') ? 'active' : '' }}" >
-                <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon servidores"></i>
-                    <span class="nav-text">Hosting <br> Servidores</span>
-                </a>
-                <div class="triangle"></div>
-            </li>
-            <li class="nav-item {{ request()->is('domains') ? 'active' : '' }}" >
-                <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon dominios"></i>
-                    <span class="nav-text">Dominios</span>
-                </a>
-                <div class="triangle"></div>
-            </li>
-            <li class="nav-item {{ request()->is('certificates') ? 'active' : '' }}" >
-                <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon certificates"></i>
-                    <span class="nav-text">Certificados</span>
-                </a>
-                <div class="triangle"></div>
-            </li>
-
             <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
                 <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon clientes"></i>
-                    <span class="nav-text">Clientes</span>
+                    <i class="nav-icon sedes"></i>
+                    <span class="nav-text">Sedes</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-
-            <li class="nav-item {{ request()->is('social-media') ? 'active' : '' }}">
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
                 <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon redes-sociales"></i>
-                    <span class="nav-text">Redes sociales</span>
+                    <i class="nav-icon recaudo"></i>
+                    <span class="nav-text">Recaudo</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-            <li class="nav-item {{ request()->is('emails') ? 'active' : '' }}">
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
                 <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon emails"></i>
-                    <span class="nav-text">Correos electrónicos</span>
+                    <i class="nav-icon factura"></i>
+                    <span class="nav-text">Facturas</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-            <li class="nav-item {{ request()->is('subscriptions') ? 'active' : '' }}">
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
                 <a class="nav-item-hold" href="{{route('home')}}">
-                    <i class="nav-icon clientes"></i>
-                    <span class="nav-text">Suscripciones</span>
+                    <i class="nav-icon realizados"></i>
+                    <span class="nav-text">Pagos <br> Realizados</span>
                 </a>
                 <div class="triangle"></div>
             </li>
-
-            <li class="nav-item {{ request()->is('help') ? 'active' : '' }}">
-                <a class="nav-item-hold" href="{{route('help')}}">
-                    <i class="nav-icon ayuda"></i>
-                    <span class="nav-text">Ayuda</span>
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
+                <a class="nav-item-hold" href="{{route('home')}}">
+                    <i class="nav-icon recibidos"></i>
+                    <span class="nav-text">Pagos <br> Recibidos</span>
+                </a>
+                <div class="triangle"></div>
+            </li>
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
+                <a class="nav-item-hold" href="{{route('home')}}">
+                    <i class="nav-icon informerecaudo"></i>
+                    <span class="nav-text">Informe de <br> Recaudo</span>
+                </a>
+                <div class="triangle"></div>
+            </li>
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
+                <a class="nav-item-hold" href="{{route('home')}}">
+                    <i class="nav-icon informefacturas"></i>
+                    <span class="nav-text">Informe de <br> Facturas</span>
+                </a>
+                <div class="triangle"></div>
+            </li>
+            <li class="nav-item {{ request()->is('clients') ? 'active' : '' }}" >
+                <a class="nav-item-hold" href="{{route('home')}}">
+                    <i class="nav-icon corte"></i>
+                    <span class="nav-text">Corte</span>
                 </a>
                 <div class="triangle"></div>
             </li>
